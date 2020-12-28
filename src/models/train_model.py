c23dd8cecdba2b567c16f95c0032685f5a7c884d
Matteo Latinov <matteo@latinov.com>
2020-12-28 21:01:09 +0100

    Add model training after tuning and evaluation on training and test set (not cross-validation).

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,17 +6,12 @@ from dotenv import find_dotenv, load_dotenv
 import numpy as np
 import pandas as pd
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVR
+
 from src.features import parse_config
 from src.models import preprocessing_pipeline, Model
-
-from sklearn.model_selection import KFold
-from sklearn.linear_model import LinearRegression, Ridge, ElasticNet
-from sklearn.svm import SVR
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor
-from sklearn.model_selection import GridSearchCV
 
 
 @click.command()
@@ -26,7 +21,7 @@ from sklearn.model_selection import GridSearchCV
 def main(input_filepath, output_filepath, config_file):
 	""" Runs data loading and cleaning and pre-processing scripts and saves data in ../processed."""
 	logger = logging.getLogger(__name__)
-	logger.info('Loading training data set, setting up pipeline, tuning and training model.')
+	logger.info('Loading training data set, setting up pipeline, tuning, training and evaluating final model.')
 
 	# Parse config file
 	config = parse_config(config_file)
@@ -34,6 +29,8 @@ def main(input_filepath, output_filepath, config_file):
 	# Load data
 	X_train = pd.read_csv(input_filepath + '/X_train.csv')
 	y_train = np.log1p(pd.read_csv(input_filepath + '/y_train.csv').values)
+	X_test = pd.read_csv(input_filepath + '/X_test.csv')
+	y_test = np.log1p(pd.read_csv(input_filepath + '/y_test.csv').values)
 
 	# Pre-processing and modeling pipeline
 	cat_features = X_train.select_dtypes(include='object').columns
@@ -44,16 +41,25 @@ def main(input_filepath, output_filepath, config_file):
 		('model', SVR())
 	])
 
-	# Model
-	kf = KFold(config['modeling']['num_folds'], shuffle=True, random_state=config['seeding']['seed']).get_n_splits(X_train.values)
+	# Tune model
+	kf = KFold(config['modeling']['num_folds'], shuffle=True,
+			   random_state=config['seeding']['seed']).get_n_splits(X_train.values)
 	param_grid = {
 		'model__C': 10. ** np.arange(-3, 3),
 		'model__gamma': 10. ** np.arange(-3, 3),
 	}
 
 	model = Model.tune(pipe, X_train, y_train.ravel(), param_grid, cv=kf)
-	print('Mean cross-validation score for {}:'.format(model.name))
-	print(np.round(np.sqrt(-model.cv_score(X_train, y_train.ravel(), config['modeling']['scoring'], kf)), 3))
+
+	# Train model
+	model.train(X_train, y_train.ravel())
+
+	# Evaluate model
+	train_preds = model.predict(X_train)
+	test_preds = model.predict(X_test)
+
+	print('Training set RSME for {} model: {}'.format(model.name, np.sqrt(mean_squared_error(y_train, train_preds))))
+	print('Test set RSME for {} model: {}'.format(model.name, np.sqrt(mean_squared_error(y_test, test_preds))))
 
 	# Save model
 	model.save(output_filepath + model.name + '.pkl')
